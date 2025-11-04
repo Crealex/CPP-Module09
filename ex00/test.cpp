4ba0b7bb0b2920bf562448e8a6d6fd75a241c904
Crealex <Alexandre.tomasi80@gmail.com>
2025-11-04 09:58:21 +0100

    continue test ctime ftcs

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -4,25 +4,25 @@
 
 int main()
 {
-	std::string stringDate("2023-04-23");
-	struct tm date;
-	struct tm temp_date;
+	std::string stringDate("2023-03-30");
+	struct tm date{};
+	struct tm after{};
 
-	std::cout << strptime(stringDate.c_str(), "%Y-%m-%d", &temp_date) << std::endl;
+	// std::cout << strptime(stringDate.c_str(), "%Y-%m-%d", &after) << std::endl;
 	if (!strptime(stringDate.c_str(), "%Y-%m-%d", &date))
 	{
 		std::cout << RED << "Invalid date format!" << RESET << std::endl;
 		return (1);
 	}
-
+	after = date;
 	std::cout << mktime(&date) << std::endl;
-	if (date.tm_year != temp_date.tm_year)
+	if (date.tm_year != after.tm_year)
 		std::cout << RED << "Invalid year => " << date.tm_year << RESET << std::endl;
-	if (date.tm_mon != temp_date.tm_mon)
+	if (date.tm_mon != after.tm_mon)
 		std::cout << RED << "Invalid month => " << date.tm_mon << RESET << std::endl;
-	if (date.tm_mday != temp_date.tm_mday)
+	if (date.tm_mday != after.tm_mday)
 		std::cout << RED << "Invalid day => " << date.tm_mon << RESET << std::endl;
-	if (date.tm_year != temp_date.tm_year || date.tm_mday != temp_date.tm_mday || date.tm_mon != temp_date.tm_mon)
+	if (date.tm_year != after.tm_year || date.tm_mday != after.tm_mday || date.tm_mon != after.tm_mon)
 	{
 		return (1);
 	}
