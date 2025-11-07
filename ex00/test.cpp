c0dddcbeff52cc154105b40ffd473d91de887465
Crealex <Alexandre.tomasi80@gmail.com>
2025-11-07 22:36:25 +0100

    continue relfexion + code test.pp (date parsing)

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -2,29 +2,38 @@
 #include <ctime>
 #include <iostream>
 
-int main()
+// REFLECHIR A COMPLETEMET CHANGER POUR UTILISER LES STRINFSTREAM /!\
+
+int main(int argc, char **argv)
 {
-	std::string stringDate("2023-02-30");
+	// std::string stringDate("2023-02-30");
 	struct tm date{};
 	struct tm after{};
 
-	// std::cout << strptime(stringDate.c_str(), "%Y-%m-%d", &after) << std::endl;
-	if (!strptime(stringDate.c_str(), "%Y-%m-%d", &date))
+	if (argc != 2)
 	{
-		std::cout << RED << "Invalid date format!" << RESET << std::endl;
+		std::cerr << RED << "Please one aurgument with 'YYYY-MM-DD'" << RESET << std::endl;
+		return (1);
+	}
+	std::cout << "Raw arguments: " << argv[1] << std::endl;
+	if (!strptime(argv[1], "%Y-%m-%d", &date))
+	{
+		std::cout << RED << "Invalid date format =>" << argv[1] << RESET << std::endl;
 		return (1);
 	}
 	after = date;
-	std::cout << mktime(&date) << std::endl;
-	if (date.tm_year != after.tm_year)
-		std::cout << RED << "Invalid year => " << after.tm_year << RESET << std::endl;
-	if (date.tm_mon != after.tm_mon)
-		std::cout << RED << "Invalid month => " << after.tm_mon << RESET << std::endl;
-	if (date.tm_mday != after.tm_mday)
-		std::cout << RED << "Invalid day => " << after.tm_mon << RESET << std::endl;
+	mktime(&date);
+	// if (date.tm_year != after.tm_year)
+	//	std::cout << RED << "Invalid year => " << after.tm_year + 1900 << RESET << std::endl;
+	// if (date.tm_mon != after.tm_mon)
+	//	std::cout << RED << "Invalid month => " << after.tm_mon + 1 << RESET << std::endl;
+	// if (date.tm_mday != after.tm_mday)
+	//	std::cout << RED << "Invalid day => " << after.tm_mday << RESET << std::endl;
 	if (date.tm_year != after.tm_year || date.tm_mday != after.tm_mday || date.tm_mon != after.tm_mon)
 	{
+		std::cerr << RED << "Invalid date => " << after.tm_year + 1900 << "-" << after.tm_mon + 1 << "-" << after.tm_mday << RESET << std::endl;
 		return (1);
 	}
+	std::cout << GREEN << "Date valid! => " << date.tm_year + 1900 << "-" << date.tm_mon + 1 << "-" << date.tm_mday << RESET << std::endl;
 	return (0);
 }
